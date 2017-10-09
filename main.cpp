2d2ae09960e99b47281f2527e79170f08689c435
J3FALL <crisp.coad@gmail.com>
2017-10-09 22:59:34 +0300

    Add array initialization

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <time.h>
+
+#define N 10000 // N * N = size of matrix
+
+void initMatrix(int **&matrix) {
+
+    srand(time(NULL));
+
+    matrix = new int *[N];
+    for (int i = 0; i < N; i++) {
+        matrix[i] = new int[N];
+        for (int j = 0; j < N; j++) {
+            matrix[i][j] = rand() % 10000;
+        }
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+    int **matrix;
+    initMatrix(matrix);
+
 }
