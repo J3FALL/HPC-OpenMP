6cfd7e6c1a6212c43b7afeb1c0d05067661cb7e7
J3FALL <crisp.coad@gmail.com>
2017-10-09 22:55:25 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/HPC_OpenMP.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HPC_OpenMP.dir/main.cpp.obj"
+  "HPC_OpenMP.pdb"
+  "HPC_OpenMP.exe"
+  "HPC_OpenMP.exe.manifest"
+  "libHPC_OpenMP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HPC_OpenMP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
